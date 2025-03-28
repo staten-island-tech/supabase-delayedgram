ba801225850f496fc729d1cf6a91c42bf82f0323
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-03-28 12:11:34 -0400

    asdfasdfasdf

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ProfileView from '../views/ProfileView.vue'
+import CreatePostView from '../views/CreatePostView.vue'
+import FollowerView from '../views/FollowerView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +18,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: ProfileView,
+    },
+    {
+      path: '/followers',
+      name: 'Followers',
+      component: FollowerView,
+    },
+    {
+      path: '/post',
+      name: 'Post',
+      component: CreatePostView,
     },
   ],
 })
