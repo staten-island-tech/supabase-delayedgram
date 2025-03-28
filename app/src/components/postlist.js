fd5182fefa9bb0c4060eada8ed7ce9399d86c5df
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-03-28 12:31:56 -0400

    asdasasdasdsdasdasdasd

---
+++ app/src/components/postlist.js
@@ -0,0 +1,2 @@
+import { reactive } from 'vue'
+const posts = reactive({})
--- app/src/views/ProfileView.vue
+++ app/src/views/ProfileView.vue
@@ -2,6 +2,9 @@
   <div><h1>Profile View</h1></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { followers } from '@/components/followerlist.js'
+import { following } from '@/components/followerlist.js'
+</script>
 
 <style lang="scss" scoped></style>
