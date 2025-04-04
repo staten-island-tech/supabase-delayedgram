f525a4cf98551060f9965abb687655365c6157c9
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2025-04-04 12:32:20 -0400

    sdfsd

---
+++ app/src/components/LoginInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <form type="text" class="loginInfo">
+      <input required class="username" type="text" pattern="[A-Za-z0-9._]+" placeholder="USERNAME" >
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -3,20 +3,23 @@ import HomeView from '../views/HomeView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import CreatePostView from '@/views/CreatePostView.vue'
 import FollowerView from '@/views/FollowerView.vue'
+import LoginPage from '@/views/LoginPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
   {
