2bc4f3780dd8f25b9fbfa60c0a84ea624bbe28a6
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-04-10 11:55:10 -0400

    asdasdfasdf

--- app/src/components/LoginInfo.vue
+++ app/src/components/LoginInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex justify-center items-center bg-gradient-to-r from-blue-500 to-pink-500">
+  <div class="min-h-screen flex justify-center items-center unpleasant-gradient">
     <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg">
       <h1 class="text-3xl font-semibold text-center text-gray-800 mb-6">LOGIN</h1>
       <form class="space-y-4">
@@ -9,14 +9,14 @@
           type="text"
           pattern="[A-Za-z0-9._]+"
           placeholder="USERNAME"
-        >
+        />
         <input
           required
           class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           type="password"
           pattern="[A-Za-z0-9._]+"
           placeholder="PASSWORD"
-        >
+        />
         <button
           type="submit"
           class="w-full py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-200"
@@ -28,7 +28,10 @@
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.unpleasant-gradient {
+  background: linear-gradient(95deg, rgb(34, 216, 36), rgb(252, 51, 242), rgb(160, 82, 6));
+}
+</style>
--- app/src/views/LoginPage.vue
+++ app/src/views/LoginPage.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>
+  <div>
     <LoginInfo />
-    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import LoginInfo from '../components/LoginInfo.vue';
+import LoginInfo from '../components/LoginInfo.vue'
 </script>
 
 <style scoped>
-
+.unpleasant-gradient {
+  background: linear-gradient(95deg, rgb(34, 216, 36), rgb(252, 51, 242), rgb(160, 82, 6));
+}
 </style>
