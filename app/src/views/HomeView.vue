9dab3bca1054454632b3b64ce01899f0211dbf9b
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-05-21 12:29:56 -0400

    adssda

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { onMounted } from 'vue'
 import { supabase } from '../supabaseclient'
 
 onMounted(async () => {
-  const { data, error } = await supabase.from('login_info').select('*').limit(1)
+  const { data, error } = await supabase.from('users').select('*').limit(1)
   if (error) {
     console.error('Connection test failed:', error.message)
   } else if (!data || data.length === 0) console.warn('No data returned')
